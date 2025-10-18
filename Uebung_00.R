a479e2a258910c73d6b49bdf66fd6785ddbb612a
QQ <yigeqinqiang@gmail.com>
2025-10-18 10:37:44 +0200

    00

---
+++ Uebung_00.R
@@ -0,0 +1,27 @@
+##1
+3 + 8
+
+x <- 18
+x
+x + 5
+
+y <- x + 5
+y
+## Aufgabe.6
+stockvalue <- 50
+growth_multiplier <- 1.2
+result <- stockvalue * growth_multiplier ^ 3
+result
+
+## Aufgabe.7
+my_vector <- c(2, 3, 4, 6)
+
+## Aufgabe.8
+my_vector + 3
+
+## A9
+my_vector[3]
+
+##A10
+my_vector <- my_vector + 3
+my_vector
