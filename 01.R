996adc37beb6e1e88eeac7bce2f1881ef67ea93b
QQ <yigeqinqiang@gmail.com>
2025-10-18 10:25:19 +0200

    erst Uebung

--- 01.R
+++ 01.R
@@ -19,3 +19,30 @@ x + y
 x - y
 x * y
 x ^ y
+x %% y
+
+z <- 8 + 2
+z
+
+x <- 8
+v <- c(198, 203, 206, 206, 213, 185)
+v
+typeof(v)
+typeof(x)
+
+v + 2
+v + c(2, -2)
+v + c(2, -2, 1, -3)
+
+heights <- c(198, 203, 206, 213, 185)
+heights[1]
+heights[5]
+heights[c(3, 5)]
+heights[c(3:5)]
+heights[c(3,4,5)]
+heights[3:5]
+heights[1:4]
+heights[-1]
+heights[-2]
+heights[-c(1,3,5)]
+heights[-(3:5)]
